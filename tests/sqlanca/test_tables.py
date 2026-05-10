f388800762c9c9967b03bf1d22a2ee61ec2af500
Pependr <antonkudrikov88@gmail.com>
2026-05-10 13:17:25 +0300

    Fix a test

--- tests/sqlanca/test_tables.py
+++ tests/sqlanca/test_tables.py
@@ -44,29 +44,3 @@ def test_table_create_error() -> None:
 
 			with pt.raises(sql.IntegrityError):
 				cur.execute("INSERT INTO test (unique_col) VALUES (NULL)")
-
-
-def test_table_create_collation() -> None:
-	def len_collation(a: str, b: str) -> int:
-		return (len(a) > len(b)) - (len(a) < len(b))
-
-	test_table = Table(
-		"test",
-		Column("collated_column", "TEXT", collation=len_collation),
-	)
-
-	with test_table.connect(":memory:") as table:
-		table.create()
-		with table.__cursor__() as cur:
-			cur.execute(
-				"INSERT INTO test (collated_column) VALUES ('ccccc'), ('aaa'), ('bbbbbbb')"
-			)
-
-			cur.execute("SELECT * FROM test")
-			out1 = cur.fetchall()
-
-			cur.execute("SELECT * FROM test ORDER BY collated_column")
-			out2 = cur.fetchall()
-
-	assert out1 == [("ccccc",), ("aaa",), ("bbbbbbb",)]
-	assert out2 == [("aaa",), ("ccccc",), ("bbbbbbb",)]
